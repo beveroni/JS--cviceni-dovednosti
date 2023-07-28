2adb56a88bf1f51d6286b624e0fdeaac97507b9c
Veronika Bendova <svechovaver@gmail.com>
2023-07-28 21:56:17 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('funguju!');
+const updateSkill = (id, uroven) => {
+  const postup = document.querySelector(`#${id} .skill__progress`);
+  const hodnota = document.querySelector(`#${id} .skill__value`);
+  hodnota.textContent = `${uroven} / 100`;
+  postup.style.width = `${uroven}%`;
+};
+
+const html = Number(prompt('Jaká je vaše úroveň HTML? (0 až 100)'));
+const css = Number(prompt('Jaká je vaše úroveň CSS? (0 až 100)'));
+const js = Number(prompt('Jaká je vaše úroveň JavaScriptu? (0 až 100)'));
+
+updateSkill('skill1', html);
+updateSkill('skill2', css);
+updateSkill('skill3', js);
